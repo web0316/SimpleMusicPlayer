72491747bc1b16d51885bf0c57f4b99df42d13cf
web0316 <webdkyw@163.com>
2013-12-05 16:48:57 +0800

    Revert "Revert "Initial Commit""
    This reverts commit adff94f663e785fb440c77192e85ab0876921a87.

---
+++ homework/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  homework
+//
+//  Created by 王 恩斯 on 13-4-18.
+//  Copyright (c) 2013年 作业. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
